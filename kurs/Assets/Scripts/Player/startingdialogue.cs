e201dd5e18df1753b6e1c04a4c4e9d23c0a98077
ermsonya <sonya.yermolova2012@mail.ru>
2022-03-28 15:25:55 +0300

    2

--- kurs/Assets/Scripts/Player/startingdialogue.cs
+++ kurs/Assets/Scripts/Player/startingdialogue.cs
@@ -5,31 +5,30 @@ using UnityEngine.SceneManagement;
 public class startingdialogue : MonoBehaviour
 {
     public Dialogue dialogue;
-
-
-
+    public double talkDistance = 0.5;
+    public GameObject player1;
+   
     void Update()
     {
-
+        // if (Input.GetKey(KeyCode.F)) {
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(transform.position, -Vector3.up, out hit) )
+        //        print("Found an object - distance: " + hit.distance);
+        //    if (hit.distance <= talkDistance)
+        //    //if (Vector3.Distance(player1.transform.position, transform.position) )
+        //    FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        //}
     }
 
-
-    void OnTriggerEnter(Collider collision)
-    {
-        Debug.Log("did0");
-        if (collision.gameObject.tag == "Player")
-        {
-            Debug.Log("did1");
-            //if (Input.GetKey(KeyCode.F))
-            //{
-            //    Debug.Log("did");
-            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-
-            //}
-        }
-    }
-        void OnTriggerExit(Collider collision)
-        {
-        //animator.SetBool("IsOpen", false);
-    }
 }
+    //void OnTriggerEnter(Collider collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    //    }
+    //}
+    //void OnTriggerExit(Collider collision)
+    //{
+    //    animator.SetBool("IsOpen", false);
+    //}
